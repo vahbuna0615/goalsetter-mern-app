8595b146abb5c1c9bada2e3acae58f19f161b69e
anubhav <anubhavadhikari15@gmail.com>
2023-05-04 18:31:46 +0530

    new proxy url updated

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,12 +1,10 @@
 import axios from 'axios'
 
-const url = 'https://goalsetter-backend-j1ew.onrender.com'
-
 const API_URL = '/api/users/'
 
 //Register user
 const register = async (userData) => {
-  const response = await axios.post(url + API_URL, userData)
+  const response = await axios.post(API_URL, userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
@@ -17,7 +15,7 @@ const register = async (userData) => {
 
 //Login user
 const login = async (userData) => {
-  const response = await axios.post(url + API_URL + 'login', userData)
+  const response = await axios.post(API_URL + 'login', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
